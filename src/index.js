239719684766c73a698cf639eaaa6e7c605800d2
Антон Чащин <anton.casin@gmail.com>
2018-03-18 20:48:11 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ export default function ({ types: t }) {
     } = state.opts;
 
     const useNew = false;
-    const constructorFunction = 'this.__changeElement';
+    const constructorFunction = 'e.__changeElement';
     const useVariables = true;
 
     let variablesRegex, jsxObjectTransformer;
